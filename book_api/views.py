258609e1bed956f0550f67abf6d04fc77915bf0d
Fatmanur Özçetin <93328444+Nurrozcetin@users.noreply.github.com>
2024-08-05 15:05:53 +0300

    get request by id

--- book_api/views.py
+++ book_api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from book_api.models import Book
 from book_api.serializer import BookSerializer
 
@@ -10,5 +11,20 @@ def book_list(request):
     serializer = BookSerializer(books, many=True)
     return Response(serializer.data)
 
-def create(request):
-    pass 
+@api_view(['GET'])
+def book(request, id):
+    try:
+        book = Book.objects.get(pk=id)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
+    except:
+        return Response({"error": "No matching record found"}, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['POST'])
+def book_create(request):
+    serializer = BookSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors)
