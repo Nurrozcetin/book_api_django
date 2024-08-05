39aff8aeb70b640cf712f2d06450ea2ce34c7111
Fatmanur Özçetin <93328444+Nurrozcetin@users.noreply.github.com>
2024-08-05 15:56:53 +0300

    optimize requests

--- book_api/urls.py
+++ book_api/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.books),
     path("<int:id>", views.book),
-    path("create/", views.book_create),
-    path("list", views.book_list),
-    path("update/<int:id>", views.book_update)
 ]
--- book_api/views.py
+++ book_api/views.py
@@ -5,36 +5,39 @@ from rest_framework import status
 from book_api.models import Book
 from book_api.serializer import BookSerializer
 
-@api_view(['GET'])
-def book_list(request):
-    books = Book.objects.all()
-    serializer = BookSerializer(books, many=True)
-    return Response(serializer.data)
+@api_view(['GET', 'POST'])
+def books(request):
+    if request.method == "GET": 
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+    if request.method == "POST":
+        serializer = BookSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors)
 
-@api_view(['GET'])
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def book(request, id):
     try:
         book = Book.objects.get(pk=id)
-        serializer = BookSerializer(book)
-        return Response(serializer.data)
+        
     except:
         return Response({"error": "No matching record found"}, status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['PUT'])
-def book_update(request, id):
-    book = Book.objects.get(pk=id)
-    serializer = BookSerializer(book, data = request.data)
-    if serializer.is_valid():
-        serializer.save()
+    if request.method == "GET":
+        serializer = BookSerializer(book)
         return Response(serializer.data)
-    else:
-        return Response(serializer.errors)
-
-@api_view(['POST'])
-def book_create(request):
-    serializer = BookSerializer(data = request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    else:
-        return Response(serializer.errors)
+    elif request.method == "PUT":
+        serializer = BookSerializer(book, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+    elif request.method == "DELETE":
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
